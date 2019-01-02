7907f8dcf600de0e32c1303cf9e282cf49aa9e4b
Rafael Barros <rafaelbarros.df@gmail.com>
2019-01-01 23:20:47 -0200

    Iterando com a diretiva ngFor

---
+++ 09.13-iterando-com-a-diretiva-ngfor/meuprojeto/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  ultimoId = 0;
+  nome = 'Rafael';
+  adicionado = false;
+  funcionarios = [];
+
+  adicionar() {
+    console.log(`Adicionando ${this.nome}`);
+    this.adicionado = true;
+    this.funcionarios.push({
+      id: ++this.ultimoId,
+      nome: this.nome
+    });
+  }
+
+}
