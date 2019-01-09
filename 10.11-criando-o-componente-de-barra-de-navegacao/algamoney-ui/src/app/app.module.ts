362de9905260ebfe4f9b931d052067882d5bc254
Rafael Barros <rafaelbarros.df@gmail.com>
2019-01-09 00:33:20 -0200

    Criando o componente de barra de navegação

---
+++ 10.11-criando-o-componente-de-barra-de-navegacao/algamoney-ui/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { ButtonModule } from 'primeng/components/button/button';
+import { TableModule } from 'primeng/components/table/table';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+
+
+
+import { AppComponent } from './app.component';
+import { LancamentosPesquisaComponent } from './components/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    ButtonModule,
+    InputTextModule,
+    TableModule,
+    TooltipModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
