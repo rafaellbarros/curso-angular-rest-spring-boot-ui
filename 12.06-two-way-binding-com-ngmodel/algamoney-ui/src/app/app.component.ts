fd755831165aacc190eb90316e71530a586d3d49
Rafael Barros <rafaelbarros.df@gmail.com>
2019-01-13 12:39:33 -0200

    Two-way binding com ngModel

---
+++ 12.06-two-way-binding-com-ngmodel/algamoney-ui/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+class Cliente {
+  nome: string;
+  email: string;
+  profissao: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  cliente = new Cliente();
+  profissoes = ['Programador', 'Empresário', 'Outra'];
+  profissao = 'Outra';
+
+  salvar(form: NgForm) {
+
+    // this.cliente.nome = form.value.nome;
+    // this.cliente.email = form.value.email;
+    // this.cliente.profissao = form.value.profissao;
+    console.log(form.value);
+    console.log(this.cliente);
+  }
+}
