2d14d619e1c3b2383cf8abfc07c01b100f1a4fcc
Rafael Barros <rafaelbarros.df@gmail.com>
2019-01-15 22:43:30 -0200

    :construction: Adicionando botão de seleção

---
+++ 13.03-adicionando-botao-de-selecao/algamoney-ui/src/app/components/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent implements OnInit {
+  tipos = [
+    { label: 'Receita', value: 'RECEITA'},
+    { label: 'Despesa', value: 'DESPESA'}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
