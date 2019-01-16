7d5913c563e60c8cd32505d3c561d05030a5bcbc
Rafael Barros <rafaelbarros.df@gmail.com>
2019-01-15 23:18:11 -0200

    :construction: Adicionando máscara de dinheiro com ngx-currency-mask

---
+++ 13.05-adicionando-mascara-de-dinheiro-com-ng2-mask-money/algamoney-ui/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ButtonModule } from 'primeng/components/button/button';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
+import { TableModule } from 'primeng/components/table/table';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { CalendarModule } from 'primeng/components/calendar/calendar';
+import { SelectButtonModule } from 'primeng/components/selectbutton/selectbutton';
+import { DropdownModule } from 'primeng/components/dropdown/dropdown';
+
+import { CurrencyMaskModule } from "ngx-currency-mask";
+
+import { AppComponent } from './app.component';
+import { LancamentoCadastroComponent } from './components/lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './components/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PessoasPesquisaComponent } from './components/pessoas-pesquisa/pessoas-pesquisa.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent,
+    PessoasPesquisaComponent,
+    LancamentoCadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    InputTextModule,
+    TableModule,
+    TooltipModule,
+    InputTextareaModule,
+    CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
+    CurrencyMaskModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
