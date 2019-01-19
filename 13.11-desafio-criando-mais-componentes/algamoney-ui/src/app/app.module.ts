dd653610bccbc928f7c4b0c2692a121198c26e42
Rafael Barros <rafaelbarros.df@gmail.com>
2019-01-19 21:00:30 -0200

    :construction: Desafio criando mais componentes

---
+++ 13.11-desafio-criando-mais-componentes/algamoney-ui/src/app/app.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { ButtonModule } from 'primeng/components/button/button';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
+import { TableModule } from 'primeng/components/table/table';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { CalendarModule } from 'primeng/components/calendar/calendar';
+import { SelectButtonModule } from 'primeng/components/selectbutton/selectbutton';
+import { DropdownModule } from 'primeng/components/dropdown/dropdown';
+import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
+import {MessageModule} from 'primeng/message';
+
+import { CurrencyMaskModule } from 'ngx-currency-mask';
+
+import { AppComponent } from './app.component';
+import { LancamentoCadastroComponent } from './components/lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './components/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PessoasPesquisaComponent } from './components/pessoas-pesquisa/pessoas-pesquisa.component';
+import { PessoaCadastroComponent } from './components/pessoa-cadastro/pessoa-cadastro.component';
+import { MessageComponent } from './components/message/message.component';
+import { LancamentosGridComponent } from './components/lancamentos-grid/lancamentos-grid.component';
+import { PessoasGridComponent } from './components/pessoas-grid/pessoas-grid.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent,
+    PessoasPesquisaComponent,
+    LancamentoCadastroComponent,
+    PessoaCadastroComponent,
+    MessageComponent,
+    LancamentosGridComponent,
+    PessoasGridComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+
+    ButtonModule,
+    InputTextModule,
+    TableModule,
+    TooltipModule,
+    InputTextareaModule,
+    CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
+    CurrencyMaskModule,
+    InputMaskModule,
+    MessageModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
