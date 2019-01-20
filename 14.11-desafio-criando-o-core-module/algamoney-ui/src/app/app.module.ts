15b1fcd4cce29a3c0ea33ba2666321b699a1b379
Rafael Barros <rafaelbarros.df@gmail.com>
2019-01-20 14:02:01 -0200

    :construction: Desafio criando o core module

---
+++ 14.11-desafio-criando-o-core-module/algamoney-ui/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core';
+import { LancamentosModule } from './modules/lancamentos';
+import { PessoasModule } from './modules/pessoas';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+
+    CoreModule,
+    LancamentosModule,
+    PessoasModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 14.11-desafio-criando-o-core-module/algamoney-ui/src/app/modules/pessoas/pessoas.module.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/components/button/button';
+import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { TableModule } from 'primeng/components/table/table';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { PessoaCadastroComponent } from './components/pessoa-cadastro';
+import { PessoasGridComponent } from './components/pessoas-grid';
+import { PessoasPesquisaComponent } from './components/pessoas-pesquisa';
+import { SharedModule } from 'src/app/shared';
+
+
+@NgModule({
+  declarations: [
+    PessoaCadastroComponent,
+    PessoasPesquisaComponent,
+    PessoasGridComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+
+    ButtonModule,
+    InputTextModule,
+    TableModule,
+    TooltipModule,
+    InputMaskModule,
+
+    SharedModule
+
+  ],
+  exports: [
+    PessoaCadastroComponent,
+    PessoasPesquisaComponent,
+  ],
+})
+export class PessoasModule { }
