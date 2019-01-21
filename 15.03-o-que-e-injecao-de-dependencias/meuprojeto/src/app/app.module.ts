53bd4b3782df3ae896d09fa18dc1b77ef5a68702
Rafael Barros <rafaelbarros.df@gmail.com>
2019-01-20 23:03:05 -0200

    :construction: O que é injecao de dependencias

---
+++ 15.03-o-que-e-injecao-de-dependencias/meuprojeto/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
+import { FuncionarioFormComponent } from './funcionario-form/funcionario-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FuncionarioCardComponent,
+    FuncionarioFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
