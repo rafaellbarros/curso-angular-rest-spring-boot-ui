1dd4da4d0830143512fdd46511637bbd6fa2d7e4
Rafael Barros <rafaelbarros.df@gmail.com>
2019-02-16 23:06:22 -0300

    :construction: Recebendo parametros da rota

---
+++ 18.05-recebendo-parametros-da-rota/algamoney-ui/src/app/core/core.module.ts
@@ -0,0 +1,33 @@
+import { CommonModule, registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { ToastyModule } from 'ng2-toasty';
+import { ConfirmationService } from 'primeng/components/common/api';
+import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
+import { NavbarComponent } from './components/navbar';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [
+    CommonModule,
+    ToastyModule.forRoot(),
+    ConfirmDialogModule,
+    SharedModule
+  ],
+  exports: [
+    NavbarComponent,
+    ToastyModule,
+    ConfirmDialogModule
+  ],
+  providers: [
+    ConfirmationService,
+    { provide: LOCALE_ID, useValue: 'pt-BR'}
+  ],
+})
+export class CoreModule { }
---
+++ 18.05-recebendo-parametros-da-rota/algamoney-ui/src/app/modules/lancamentos/components/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { CategoriaService } from '@app/categorias';
+import { ErrorHandlerService } from '@app/core/services/error-handler.service';
+import { Lancamento } from '@lancamentos/models';
+import { LancamentoService } from '@lancamentos/services';
+import { PessoaService } from '@pessoas/services';
+import { ToastyService } from 'ng2-toasty';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent implements OnInit {
+
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' },
+  ];
+
+  categorias = [];
+  pessoas = [];
+  lancamento = new Lancamento();
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private categoriaService: CategoriaService,
+    private pessoaService: PessoaService,
+    private lancamentoService: LancamentoService,
+    private toaty: ToastyService,
+    private errorHandler: ErrorHandlerService) { }
+
+  ngOnInit() {
+    const codigo = this.route.snapshot.params['codigo'];
+    console.log(codigo);
+
+    this.carregaCategorias();
+    this.carregaPessoas();
+  }
+
+  salvar(form: FormControl) {
+    this.lancamentoService.adicionar(this.lancamento).subscribe(resp => {
+      this.toaty.success('Lançamento adicionando com sucesso!');
+
+      form.reset();
+      this.lancamento = new Lancamento();
+    }, error => this.errorHandler.handle(error));
+  }
+
+  private carregaCategorias = () => this.categoriaService
+    .listarTodas()
+      .subscribe(
+        categorias => this.categorias = categorias,
+        error => this.errorHandler.handle(error)
+      )
+
+  private carregaPessoas = () => this.pessoaService
+    .listarTodas()
+      .subscribe(
+        pessoas => this.pessoas = pessoas.map(pessoa => ({label: pessoa.nome, value: pessoa.codigo})),
+        error => this.errorHandler.handle(error)
+      )
+}
