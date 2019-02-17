4dd730ddd4c706d33763928bd2ce200904b3d174
Rafael Barros <rafaelbarros.df@gmail.com>
2019-02-17 02:23:39 -0300

    :construction: Fazendo redirecionamento

---
+++ 18.10-fazendo-redirecionamento/algamoney-ui/src/app/modules/lancamentos/components/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { CategoriaService } from '@app/categorias';
+import { ErrorHandlerService } from '@app/core/services/error-handler.service';
+import { Lancamento } from '@lancamentos/models';
+import { LancamentoService } from '@lancamentos/services';
+import { PessoaService } from '@pessoas/services';
+import { ToastyService } from 'ng2-toasty';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent implements OnInit {
+
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' },
+  ];
+
+  categorias = [];
+  pessoas = [];
+  lancamento = new Lancamento();
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private categoriaService: CategoriaService,
+    private pessoaService: PessoaService,
+    private lancamentoService: LancamentoService,
+    private toaty: ToastyService,
+    private errorHandler: ErrorHandlerService) { }
+
+  ngOnInit() {
+
+    const { codigo } = this.route.snapshot.params;
+
+    if (codigo) { this.carregarLancamento(codigo); }
+
+    this.carregarCategorias();
+    this.carregarPessoas();
+  }
+
+  salvar(form: FormControl) {
+    if (this.editando) {
+      this.atualizarLancamento(form);
+    } else {
+      this.adicionarLancamento(form);
+    }
+  }
+
+
+  adicionarLancamento(form: FormControl) {
+    this.lancamentoService.adicionar(this.lancamento).subscribe(lancamento => {
+      this.toaty.success('Lançamento adicionando com sucesso!');
+
+      // form.reset();
+      // this.lancamento = new Lancamento();
+
+      this.router.navigate(['/lancamentos', lancamento.codigo]);
+    }, error => this.errorHandler.handle(error));
+  }
+
+  atualizarLancamento(form: FormControl) {
+    this.lancamentoService.atualizar(this.lancamento).subscribe(lancamento => {
+      this.lancamento = lancamento;
+
+      this.toaty.success('Lançamento alterado com sucesso!')
+    }, error => this.errorHandler.handle(error));
+  }
+
+  novo(form: FormControl) {
+    form.reset();
+
+    setTimeout(function () {
+      this.lancamento = new Lancamento();
+    }.bind(this), 1)
+
+    this.router.navigate(['/lancamentos/novo']);
+  }
+
+  get editando() {
+    return Boolean(this.lancamento.codigo);
+  }
+
+  private carregarLancamento = (codigo: number) =>
+    this.lancamentoService.buscarPorCodigo(codigo)
+      .subscribe(lancamento => this.lancamento = lancamento)
+
+  private carregarCategorias = () => this.categoriaService
+    .listarTodas()
+      .subscribe(
+        categorias => this.categorias = categorias,
+        error => this.errorHandler.handle(error)
+      )
+
+  private carregarPessoas = () => this.pessoaService
+    .listarTodas()
+      .subscribe(
+        pessoas => this.pessoas = pessoas.map(pessoa => ({label: pessoa.nome, value: pessoa.codigo})),
+        error => this.errorHandler.handle(error)
+      )
+
+
+}
