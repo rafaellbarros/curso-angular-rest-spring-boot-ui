ce060fb73a63e7c5f9b8c1f45ef9778d74df238a
Rafael Barros <rafaelbarros.df@gmail.com>
2019-02-23 19:01:10 -0300

    :construction: Obtendo um novo access token

---
+++ 19.10-obtendo-um-novo-access-token/algamoney-ui/src/app/core/components/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@app/modules/seguranca/services';
+
+
+@Component({
+  selector: 'navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private auth: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  criarNovoAccessToken() {
+    this.auth.obterNovoAccessToken().subscribe(() => {
+
+    });
+  }
+
+}
---
+++ 19.10-obtendo-um-novo-access-token/algamoney-ui/src/app/modules/seguranca/services/auth.service.ts
@@ -0,0 +1,91 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+import { Utils } from '@app/core/utils';
+import { IAuthService } from './iauth.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService implements IAuthService {
+
+  oauthTokenUrl = 'http://localhost:8080/oauth/token';
+  jwtPayload: any;
+
+
+  constructor(
+    private http: HttpClient,
+    private jwtHelper: JwtHelperService) {
+      this.carregaToken();
+    }
+
+  public get options() {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic YW5ndWxhcjpAbmd1bEByMA=='
+    });
+
+    return {
+      headers: headers,
+      withCredentials: true
+    };
+  }
+
+  login(usuario: string, senha: string): Observable<void> {
+
+    const options =  this.options;
+    const body = `username=${usuario}&password=${senha}&grant_type=password`;
+
+    return this.http.post(this.oauthTokenUrl, body, options).pipe(map(resp => {
+
+      const { access_token } = Utils.json(resp);
+      this.armazenarToken(access_token);
+
+    }), catchError(resp => {
+      if (resp.status === 400) {
+        if (resp.error.error === 'invalid_grant') {
+          return throwError('Usuário ou senha inválida');
+        }
+      }
+      return throwError(resp);
+    }));
+  }
+
+  obterNovoAccessToken(): Observable<void> {
+
+    const options =  this.options;
+    const body = 'grant_type=refresh_token';
+
+    return this.http.post(this.oauthTokenUrl, body, options).pipe(map(resp => {
+        const { access_token } = Utils.json(resp);
+        this.armazenarToken(access_token);
+        console.log('Novo access token criado!');
+        return of(null);
+    }), catchError(resp => {
+        console.error('Erro ao renovar tokenn.', resp);
+        return of(null);
+    }));
+  }
+
+  temPermissao(permissao: string) {
+    return this.jwtPayload && this.jwtPayload.authorities.includes(permissao);
+  }
+
+  private armazenarToken(token: string) {
+    this.jwtPayload = this.jwtHelper.decodeToken(token);
+    localStorage.setItem('token', token);
+  }
+
+  private carregaToken() {
+    const token = this.jwtHelper.tokenGetter();
+
+    if (token) {
+      this.armazenarToken(token);
+    }
+  }
+}
