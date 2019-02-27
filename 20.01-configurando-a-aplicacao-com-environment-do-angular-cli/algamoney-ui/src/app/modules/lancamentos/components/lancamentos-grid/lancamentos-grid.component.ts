7c8def133003395b85095b82b7870f07df445c76
Rafael Barros <rafaelbarros.df@gmail.com>
2019-02-27 00:38:12 -0300

    :construction: Configurando a aplicação com enviroment do angular-cli

---
+++ 20.01-configurando-a-aplicacao-com-environment-do-angular-cli/algamoney-ui/src/app/modules/lancamentos/components/lancamentos-grid/lancamentos-grid.component.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, Input, Output, AfterViewInit, ViewChild } from '@angular/core';
+import { LazyLoadEvent } from 'primeng/components/common/api';
+import { LancamentoFiltro } from '../../models/lancamento-filtro.model';
+import { Lancamento } from '../../models';
+import { AuthService } from '../../../seguranca/services/auth.service';
+
+
+@Component({
+  selector: 'lancamentos-grid',
+  templateUrl: './lancamentos-grid.component.html',
+  styleUrls: ['./lancamentos-grid.component.css']
+})
+export class LancamentosGridComponent  implements AfterViewInit {
+
+  @ViewChild('tabela') grid;
+
+  @Input() lancamentos = [];
+
+  @Input() filtro: LancamentoFiltro;
+
+  @Input() totalRegistros: number;
+
+  @Output() eventAoMudarPagina: EventEmitter<any> = new EventEmitter();
+
+  @Output() eventConfirmarExclusao: EventEmitter<any> = new EventEmitter();
+
+  @Output() eventViewChildTabela: EventEmitter<any> = new EventEmitter();
+
+  constructor(private auth: AuthService) {
+
+  }
+
+  ngAfterViewInit() {
+    this.eventViewChildTabela.emit(this.grid);
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    this.eventAoMudarPagina.emit(event);
+  }
+
+  confirmarExclusao(lancamento: Lancamento) {
+    this.eventConfirmarExclusao.emit(lancamento);
+  }
+}
