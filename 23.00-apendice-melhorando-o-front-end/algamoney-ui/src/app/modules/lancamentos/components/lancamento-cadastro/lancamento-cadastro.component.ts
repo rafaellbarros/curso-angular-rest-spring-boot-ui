7864ab69c015a7a8738f908b663c74c5f551eb7d
Rafael Barros <rafaelbarros.df@gmail.com>
2019-06-08 19:09:45 -0300

    :construction: 23.24 - Salvando e removendo anexo

--- 23.00-apendice-melhorando-o-front-end/algamoney-ui/src/app/modules/lancamentos/components/lancamento-cadastro/lancamento-cadastro.component.ts
+++ 23.00-apendice-melhorando-o-front-end/algamoney-ui/src/app/modules/lancamentos/components/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -75,6 +75,13 @@ export class LancamentoCadastroComponent implements OnInit {
     this.uploadEmAndamento = false;
   }
 
+  removerAnexo() {
+    this.formulario.patchValue({
+      anexo: null,
+      urlAnexo: null,
+    });
+  }
+
   get nomeAnexo() {
     const nome = this.formulario.get('anexo').value;
 
